18a4d50c5c7e729829d5f2a7d02c12e4c9703ae3
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-23 20:19:03 +0300

    Lesson_9 Переписали геттеры и сеттеры с помощью декоратора property

--- lessons/lesson_9.py
+++ lessons/lesson_9.py
@@ -7,28 +7,40 @@ class Dog:
     def __check_age(cls, age):
         if type(age) != int:
             raise TypeError("Введен некорректный возраст")
-        elif cls.__MIN_AGE <= age <= cls.__MAX_AGE:
+        elif not cls.__MIN_AGE <= age <= cls.__MAX_AGE:
             raise TypeError("Возраст не может быть меньше нуля и больше 40")
         else:
             return age
 
+    @classmethod
+    def __check_name(cls, name):
+        if type(name) != str:
+            raise TypeError("Имя может быть только строкой")
+        else:
+            return name
+
     def __init__(self, name, age):
-        self.__dog_name = name
+        self.__dog_name = self.__check_name(name)
         self.__age = self.__check_age(age)
 
-    def set_name(self, name):
-        self.__dog_name = name
-
-    def set_age(self, age):
-        self.__age = self.__check_age(age)
-
-    def get_name(self):
+    @property
+    def name(self):
         return self.__dog_name
 
-    def get_age(self):
+    @name.setter
+    def name(self, name):
+        self.__dog_name = self.__check_name(name)
+
+    @property
+    def age(self):
         return self.__age
 
+    @age.setter
+    def age(self, age):
+        self.__age = self.__check_age(age)
 
-dog = Dog("Тузик", 1)
 
-print(dog.get_age())
+dog = Dog("Тузик", 40)
+print(dog.name, dog.age)
+dog.age = 15
+print(dog.name, dog.age)
