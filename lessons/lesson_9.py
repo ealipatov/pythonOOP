3e5c0758cb055c0835d95ec22c1e81f97ff740fb
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-23 19:50:37 +0300

    Lesson_9 Добавили проверки

--- lessons/lesson_9.py
+++ lessons/lesson_9.py
@@ -1,13 +1,26 @@
 class Dog:
+
+    __MIN_AGE = 0
+    __MAX_AGE = 40
+
+    @classmethod
+    def __check_age(cls, age):
+        if type(age) != int:
+            raise TypeError("Введен некорректный возраст")
+        elif cls.__MIN_AGE <= age <= cls.__MAX_AGE:
+            raise TypeError("Возраст не может быть меньше нуля и больше 40")
+        else:
+            return age
+
     def __init__(self, name, age):
         self.__dog_name = name
-        self.__age = age
+        self.__age = self.__check_age(age)
 
     def set_name(self, name):
         self.__dog_name = name
 
     def set_age(self, age):
-        self.__age = age
+        self.__age = self.__check_age(age)
 
     def get_name(self):
         return self.__dog_name
@@ -16,6 +29,6 @@ class Dog:
         return self.__age
 
 
-dog = Dog("Тузик", 5)
+dog = Dog("Тузик", 1)
 
-print(dog.get_name())
+print(dog.get_age())
