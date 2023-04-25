9668062d88aeafebbc48bdf116cebaaa7cf90138
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-25 12:20:22 +0300

    Lesson_16. Написали проверку валидности координат.

--- lessons/lesson_16.py
+++ lessons/lesson_16.py
@@ -1,17 +1,20 @@
 class Point:
+    MIN_VALUE = 0
+    MAX_VALUE = 100
+
     def __init__(self, x, y, z):
         self.set_x(x)
         self.set_y(y)
         self.set_z(z)
 
     def set_x(self, x):
-        self._x = x
+        self._x = self.validate_coordinate(x)
 
     def set_y(self, y):
-        self._y = y
+        self._y = self.validate_coordinate(y)
 
     def set_z(self, z):
-        self._z = z
+        self._z = self.validate_coordinate(z)
 
     def get_x(self):
         return self._x
@@ -21,3 +24,14 @@ class Point:
 
     def get_z(self):
         return self._z
+
+    @classmethod
+    def validate_coordinate(cls, coordinate):
+        if type(coordinate) == int and cls.MIN_VALUE <= coordinate <= cls.MAX_VALUE:
+            return coordinate
+        else:
+            return 0
+
+
+point = Point(1, 2, "3")
+print(point.get_x(), point.get_y(), point.get_z())
