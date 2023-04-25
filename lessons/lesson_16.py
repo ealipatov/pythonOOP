0afd85cbdbd70cea2c63a6ab09ad6cf3dc121b76
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-25 12:36:25 +0300

    Lesson_16. Написали дескриптор для координат.

--- lessons/lesson_16.py
+++ lessons/lesson_16.py
@@ -1,29 +1,6 @@
-class Point:
+class IntegerCoordinate:
     MIN_VALUE = 0
     MAX_VALUE = 100
-
-    def __init__(self, x, y, z):
-        self.set_x(x)
-        self.set_y(y)
-        self.set_z(z)
-
-    def set_x(self, x):
-        self._x = self.validate_coordinate(x)
-
-    def set_y(self, y):
-        self._y = self.validate_coordinate(y)
-
-    def set_z(self, z):
-        self._z = self.validate_coordinate(z)
-
-    def get_x(self):
-        return self._x
-
-    def get_y(self):
-        return self._y
-
-    def get_z(self):
-        return self._z
 
     @classmethod
     def validate_coordinate(cls, coordinate):
@@ -32,6 +9,26 @@ class Point:
         else:
             return 0
 
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
 
-point = Point(1, 2, "3")
-print(point.get_x(), point.get_y(), point.get_z())
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        return setattr(instance, self.name, self.validate_coordinate(value))
+
+
+class Point:
+    x = IntegerCoordinate()
+    y = IntegerCoordinate()
+    z = IntegerCoordinate()
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+point = Point(1, 2, "asd")
+print(point.x, point.y, point.z)
