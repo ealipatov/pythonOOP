aba60c2b83705a62b7f95a877a9f0dfc95740734
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-04-25 07:57:23 +0300

    Lesson_12 Применили декоратор

--- lessons/lesson_12.py
+++ lessons/lesson_12.py
@@ -1,24 +1,36 @@
 def hands_washer(func):  # На вход принимаем функцию
     def inner(*args, **kwargs):
         print("Моем руки")
-        func(*args, **kwargs)
+        res = func(*args, **kwargs)  # Если надо вернуть результат выполнения функциии
         print("Моем руки")
+        return res  # Возвращаем результат выполнения функции
 
     return inner
 
 
-def eat(meal1, meal2):
-    print(f"Кушаю: {meal1} и {meal2}")
-
-
 def eat_2(func, *args):
     print("Моем руки")
-    func(*args)
+    res = func(*args)
     print("Моем руки")
+    return res
 
 
-hands_washer(eat)("Мясо", "Сыр")  # Сокращенное написание:
-# clear_eat = hands_washer(eat)
-# clear_eat()
+@hands_washer
+def eat(meal1, meal2):
+    print(f"Кушаю: {meal1} и {meal2}")
+    return "Я покушал"
 
-eat_2(eat, "Сало", "Хлеб")
+
+# @eat_2  # не работает
+# def eat(meal1, meal2):
+#     print(f"Кушаю: {meal1} и {meal2}")
+#     return "Я покушал"
+#
+
+print(eat("Мясо", "Сыр"))
+
+# print(hands_washer(eat)("Мясо", "Сыр"))  # Сокращенное написание:
+# # clear_eat = hands_washer(eat)
+# # clear_eat()
+# print("=" *80)
+# print(eat_2(eat, "Сало", "Хлеб"))
