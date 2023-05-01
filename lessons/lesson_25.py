b89aee0b93d90a8e5a9d1a2a69a6278b5f29cd33
Evgeniy Alipatov <96283304+ealipatov@users.noreply.github.com>
2023-05-01 22:31:49 +0300

    Lesson_25. Посмотрели возможности дата класса.

--- lessons/lesson_25.py
+++ lessons/lesson_25.py
@@ -1,10 +1,17 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
-@dataclass()
+@dataclass(frozen=True)  # frozen=True - запрет на изменение данных в классе, можно без нее.
 class Person:
     name: str
     age: int
+    childs: list = field(default_factory=list)
 
 
-print(Person("Alex", 25))
+person1 = Person("Alex", 25)
+person2 = Person("Ben", 30)
+print(person1)
+print(person2)
+person1.childs.append("Lusy")
+print(person1)
+print(person2)
