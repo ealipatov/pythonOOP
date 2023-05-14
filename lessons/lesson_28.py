554f5e3050355bc09c0b366bcccb451b93fd8f38
Evgeniy Alipatov <ealipatov@gmail.com>
2023-05-14 16:59:22 +0300

    Lesson_28. Генератор словарей

--- lessons/lesson_28.py
+++ lessons/lesson_28.py
@@ -15,7 +15,7 @@ def plus_2(elem):
 
 
 # генератор списка с функцией в логике:
-print([plus_2(elem) for elem in list1])
+print([plus_2(elem) for elem in list1 if elem < 10])
 
 # генератор списка:
 list3 = [elem * elem for elem in list1]
@@ -23,3 +23,14 @@ print(list3)
 
 # запись с помощью лямбды
 print(list(map(lambda el: plus_2(el), list1)))
+
+# генератор словарей
+dict1 = {elem: elem * elem for elem in list1}
+print(dict1)
+
+data = ["Молоко", "Кефир", "Масло"]
+
+dict2 = {elem: len(elem) for elem in data}
+dict3 = {elem: len(elem) for elem in data if len(elem) > 5}
+print(dict2, "\n", dict3)
+print({key.upper(): value for key, value in dict2.items()})
